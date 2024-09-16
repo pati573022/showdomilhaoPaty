f4cc3f65c0aacb4a8a2b25bc8aa9fd893f3fd548
paty <patiangelita15@gmail.com>
2024-09-16 09:32:37 -0300

    paaaaaatyyy

---
+++ showdomilhaoPaty/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace showdomilhaoPaty;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ showdomilhaoPaty/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace showdomilhaoPaty;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
